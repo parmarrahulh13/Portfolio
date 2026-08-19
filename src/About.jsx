e1ca6dc260716a3afb32b7a588a2ab0d28260c28
Rahul <parmarrahulh13@gmail.com>
2026-08-19 22:36:13 +0530

    about section create

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import "./About.css";
+import heroimg from "./assets/hero-img.png";
+
+function About() {
+  return (
+    <section
+      className="About"
+      style={{ backgroundImage: `url(${heroimg})` }}
+    >
+      
+    </section>
+  );
+}
+
+export default About;
